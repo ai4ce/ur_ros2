d721635f3f0a524137506ac1abdeb71ca0a89d9a
Irving Fang <zichuanfang2015@yahoo.com>
2024-08-23 00:05:00 -0400

    C++ server now running

--- ur_planning/src/ur_planning_server.cpp
+++ ur_planning/src/ur_planning_server.cpp
@@ -1,8 +1,13 @@
+#include <rclcpp/service.hpp>
+#include <rclcpp/node.hpp>
+
 #include <moveit/move_group_interface/move_group_interface.h>
 #include <moveit/planning_scene_interface/planning_scene_interface.h>
 
 #include <moveit_msgs/msg/display_robot_state.hpp>
 #include <moveit_msgs/msg/display_trajectory.hpp>
+
+#include <thread>
 
 #include "ur_custom_msgs/srv/move_to.hpp"
 
@@ -11,81 +16,86 @@
 // and inside the namespace with the narrowest scope (if there is one)
 static const rclcpp::Logger LOGGER = rclcpp::get_logger("ur_planning_server");
 
+class URPlanningServer : public rclcpp::Node
+{
+  public:
+    URPlanningServer(const rclcpp::Node::SharedPtr move_group_node)
+    : Node("ur_planning_server"), 
+      move_group_node(move_group_node),
+      move_group(move_group_node, "arm")
+    {
+      ////////////////////////// MoveIt Setup //////////////////////////////////////////////////////////
+      
+      // We spin up a SingleThreadedExecutor for the current state monitor to get information
+      // about the robot's state.
+      rclcpp::executors::SingleThreadedExecutor executor;
+      executor.add_node(move_group_node);
+      std::thread([&executor]() { executor.spin(); }).detach();
+
+      static const std::string PLANNING_GROUP = "arm";
+
+      // The move group interface
+      move_group = moveit::planning_interface::MoveGroupInterface(move_group_node, PLANNING_GROUP);
+
+      // move_group_interface = move_group;
+      // Planning scene will be used for collision setup
+      
+
+      // Raw pointers are frequently used to refer to the planning group for improved performance.
+      joint_model_group = move_group.getCurrentState()->getJointModelGroup(PLANNING_GROUP);
+
+
+      ////////////////////////// Service Setup //////////////////////////////////////////////////////////
+      moveto_service = this->create_service<ur_custom_msgs::srv::MoveTo>(
+        "ur_planning/moveto",
+        std::bind(&URPlanningServer::moveto_callback, this, std::placeholders::_1, std::placeholders::_2)
+      );
+      
+
+      // We can print the name of the reference frame for this robot.
+      RCLCPP_INFO(LOGGER, "Planning frame: %s", move_group.getPlanningFrame().c_str());
+
+      // We can also print the name of the end-effector link for this group.
+      RCLCPP_INFO(LOGGER, "End effector link: %s", move_group.getEndEffectorLink().c_str());
+    }
+    
+  private:
+    rclcpp::Node::SharedPtr move_group_node;
+    const moveit::core::JointModelGroup* joint_model_group;
+    geometry_msgs::msg::Pose target_pose;
+    moveit::planning_interface::MoveGroupInterface::Plan my_plan;
+    rclcpp::Service<ur_custom_msgs::srv::MoveTo>::SharedPtr moveto_service;
+    moveit::planning_interface::MoveGroupInterface move_group;
+    moveit::planning_interface::PlanningSceneInterface planning_scene_interface;
+
+    void moveto_callback(const std::shared_ptr<ur_custom_msgs::srv::MoveTo::Request> request,
+                          std::shared_ptr<ur_custom_msgs::srv::MoveTo::Response> response)
+    {
+      RCLCPP_INFO(LOGGER, "Received request to move to pose");
+      target_pose = request->target_pose;
+      move_group.setPoseTarget(target_pose);
+
+      bool success = (this->move_group.plan(my_plan) == moveit::core::MoveItErrorCode::SUCCESS);
+
+      if (!success)
+      {
+        response->success = false;
+        return;
+      }
+      
+      this->move_group.move(); 
+      response->success = true;
+    }
+};
+
 int main(int argc, char** argv)
 {
   rclcpp::init(argc, argv);
   rclcpp::NodeOptions node_options;
   node_options.automatically_declare_parameters_from_overrides(true);
-  auto move_group_node = rclcpp::Node::make_shared("ur_planning_server", node_options);
+  auto move_group_node = rclcpp::Node::make_shared("ur_planning_move_group", node_options);
 
-  // We spin up a SingleThreadedExecutor for the current state monitor to get information
-  // about the robot's state.
-  rclcpp::executors::SingleThreadedExecutor executor;
-  executor.add_node(move_group_node);
-  std::thread([&executor]() { executor.spin(); }).detach();
-
-
-  // MoveIt operates on sets of joints called "planning groups" and stores them in an object called
-  // the ``JointModelGroup``. Throughout MoveIt, the terms "planning group" and "joint model group"
-  // are used interchangeably.
-  static const std::string PLANNING_GROUP = "arm";
-
-  // The
-  // :moveit_codedir:`MoveGroupInterface<moveit_ros/planning_interface/move_group_interface/include/moveit/move_group_interface/move_group_interface.h>`
-  // class can be easily set up using just the name of the planning group you would like to control and plan for.
-  moveit::planning_interface::MoveGroupInterface move_group(move_group_node, PLANNING_GROUP);
-
-  // We will use the
-  // :moveit_codedir:`PlanningSceneInterface<moveit_ros/planning_interface/planning_scene_interface/include/moveit/planning_scene_interface/planning_scene_interface.h>`
-  // class to add and remove collision objects in our "virtual world" scene
-  moveit::planning_interface::PlanningSceneInterface planning_scene_interface;
-
-  // Raw pointers are frequently used to refer to the planning group for improved performance.
-  const moveit::core::JointModelGroup* joint_model_group =
-      move_group.getCurrentState()->getJointModelGroup(PLANNING_GROUP);
-
-
-  // We can print the name of the reference frame for this robot.
-  RCLCPP_INFO(LOGGER, "Planning frame: %s", move_group.getPlanningFrame().c_str());
-
-  // We can also print the name of the end-effector link for this group.
-  RCLCPP_INFO(LOGGER, "End effector link: %s", move_group.getEndEffectorLink().c_str());
-
-  // We can get a list of all the groups in the robot:
-  RCLCPP_INFO(LOGGER, "Available Planning Groups:");
-  std::copy(move_group.getJointModelGroupNames().begin(), move_group.getJointModelGroupNames().end(),
-            std::ostream_iterator<std::string>(std::cout, ", "));
-
-
-  // get the current pose
-  geometry_msgs::msg::PoseStamped current_pose = move_group.getCurrentPose();
-  
-
-  geometry_msgs::msg::Pose target_pose1;
-  
-  // set the target pose as the current pose but with a different z value
-  target_pose1 = current_pose.pose;
-  target_pose1.position.z += 0.1;
-  
-  move_group.setPoseTarget(target_pose1);
-
-  // Now, we call the planner to compute the plan and visualize it.
-  // Note that we are just planning, not asking move_group
-  // to actually move the robot.
-  moveit::planning_interface::MoveGroupInterface::Plan my_plan;
-
-  bool success = (move_group.plan(my_plan) == moveit::core::MoveItErrorCode::SUCCESS);
-
-  RCLCPP_INFO(LOGGER, "Visualizing plan 1 (pose goal) %s", success ? "" : "FAILED");
-
-  // Visualizing plans
-  // ^^^^^^^^^^^^^^^^^
-  // We can also visualize the plan as a line with markers in RViz.
-  RCLCPP_INFO(LOGGER, "Visualizing plan 1 as trajectory line");
-
-  // Moving to a pose goal
-  move_group.move(); 
-
+  URPlanningServer ur_planning_server(move_group_node);
 
   rclcpp::shutdown();
   return 0;
